27a7cd7169a82df80c62707ecefe1426eea8cadd
mohammadrafeakhan24 <91667946+mohammadrafeakhan24@users.noreply.github.com>
2022-10-01 22:48:50 +0530

    Update list.py

--- list.py
+++ list.py
@@ -2,6 +2,8 @@ print("List of some youtube videos")
 link1 = {
 "Lec1": "https://www.youtube.com/watch?v=1pIRKUrq-ZY"
 "Lec2": "https://www.youtube.com/watch?v=ZgccHLOj3W4&list=PLEKP9nwe-ZL9NGF01fEPfTUvkrcptRl5I&index=1"
+  "Lec3": "https://www.youtube.com/watch?v=1pIRKUrq-ZY"
+"Lec4": "https://www.youtube.com/watch?v=ZgccHLOj3W4&list=PLEKP9nwe-ZL9NGF01fEPfTUvkrcptRl5I&index=1"
 }
 
 print(link1)
