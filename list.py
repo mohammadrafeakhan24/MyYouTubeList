86ce51ae7d2fd73579143c720ac91c78c0c7a960
Ayush Kumar Singh <aayushsingh.code@gmail.com>
2022-10-01 19:55:33 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+print("List of some youtube videos")
+link1 = {
+"Lec1": "https://www.youtube.com/watch?v=1pIRKUrq-ZY"
+"Lec2": "https://www.youtube.com/watch?v=ZgccHLOj3W4&list=PLEKP9nwe-ZL9NGF01fEPfTUvkrcptRl5I&index=1"
+}
+
+print(link1)
